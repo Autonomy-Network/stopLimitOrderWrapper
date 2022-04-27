54d81abf743c7f081ec15b35fa7910229bcfb45c
unbreakablery <unbreakablery@gmail.com>
2022-04-28 00:35:00 +0900

    add: chainlinkV2 test

--- test/chainlinkOracleV2.js
+++ test/chainlinkOracleV2.js
@@ -1,24 +1,26 @@
 require('dotenv').config();
 const { ethers } = require("hardhat");
-const { defaultAbiCoder } = require('@ethersproject/abi')
+const { defaultAbiCoder } = require('@ethersproject/abi');
+const { expect } = require('chai');
 
-const ChainLinkV1Address = process.env.CHAINLINK_ORACLE_V1_ADDRESS
+const ChainLinkV2Address = process.env.CHAINLINK_ORACLE_V2_ADDRESS
 const testOracleData = "0x000000000000000000000000ebe676ee90fe1112671f19b6b7459bc678b67e8a0000000000000000000000000a77230d17318075983913bc2145db16c73661560000000000000000000000000000000000000000000000000de0b6b3a7640000"
 
 describe("Oracle peek()", function () {
     it("Should return valid price feed", async () => {
         provider = await ethers.getDefaultProvider("http://localhost:8545");
-        chainlinkV1Contract= await ethers.getContractAt("IOracle", ChainLinkV1Address)
+        chainlinkV2Contract= await ethers.getContractAt("IOracle", ChainLinkV2Address)
 
         oracleData = defaultAbiCoder.encode(
             ['address', 'address', 'uint256'], // multiply, divide, decimals
             [
                 '0xEBE676ee90Fe1112671f19b6B7459bC678B67e8a',
                 '0x0A77230d17318075983913bC2145DB16C7366156',
-                '1000000000'
+                '1000000000000000000'
             ]
         )
-        oracleReturnData = await chainlinkV1Contract.peek(oracleData)
+        expect(oracleData).to.be.equal(testOracleData)
+        oracleReturnData = await chainlinkV2Contract.peek(oracleData)
     
         console.log(JSON.stringify(oracleReturnData))
         console.log("success: ", oracleReturnData[0])
