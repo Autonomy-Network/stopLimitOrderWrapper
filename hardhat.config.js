8c9039f67e906fc30497f435f99b302ab25f0214
unbreakablery <unbreakablery@gmail.com>
2022-05-02 23:42:47 +0900

    add verify script

--- hardhat.config.js
+++ hardhat.config.js
@@ -1,4 +1,5 @@
 require("@nomiclabs/hardhat-waffle");
+require("@nomiclabs/hardhat-etherscan");
 require("dotenv").config();
 
 
@@ -43,5 +44,8 @@ module.exports = {
       gasPrice: "auto",
       accounts: [process.env.PRIVATE_KEY]
     }
+  },
+  etherscan: {
+    apiKey: process.env.ETHERSCAN_API_KEY
   }
 };
